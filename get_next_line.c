60f0db82d47505e574c49fcde2697dd5b15e2280
VADE <itwrkz@gmail.com>
2019-02-21 14:26:41 +0100

    Complete & Finished GNL

--- get_next_line.c
+++ get_next_line.c
@@ -10,9 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <fcntl.h>
 #include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 static t_list	*get_file(int fd, t_list **file)
 {
@@ -25,81 +26,92 @@ static t_list	*get_file(int fd, t_list **file)
 			return (temp);
 		temp = temp->next;
 	}
-	temp = ft_lstnew("\0", 1);
+	temp = ft_lstnew("", 1);
 	temp->content_size = fd;
 	ft_lstadd(file, temp);
 	temp = *file;
 	return (temp);
 }
 
-static void		ft_read(int ret, int fd, char *buf, t_list *current)
+static int		ft_read(int fd, char *buf, t_list *current)
 {
-	char *temp;
+	char	*temp;
+	int		ret;
 
-	while (ret)
+	ret = 1;
+	while (ret > 0)
 	{
 		ret = read(fd, buf, BUFF_SIZE);
 		buf[ret] = '\0';
-		temp = ft_strdup(current->content);
+		temp = current->content;
 		current->content = ft_strjoin(current->content, buf);
 		free(temp);
-		if (ft_strchr(buf, '\n'))
+		if (ft_strchr(current->content, '\n'))
 			break ;
 	}
+	return (ret);
 }
 
 int				get_next_line(const int fd, char **line)
 {
+	int				ret;
 	char			buf[BUFF_SIZE + 1];
 	static t_list	*file;
-	int ret;
 	t_list			*current;
+	char			*temp;
 
-	if (!*line || fd < 0)
+	if (line == NULL || fd < 0 || read(fd, buf, 0) < 0)
 		return (-1);
 	current = get_file(fd, &file);
-	*line = ft_strnew(1);
-	ret = 1;
-	ft_read(ret, fd, buf, current);
+	ret = ft_read(fd, buf, current);
 	if (ret < BUFF_SIZE && !ft_strlen(current->content))
-	{
-		free(*line);
 		return (0);
-	}
-	*ft_strchr(current->content, '\n') = '\0';
 	*line = ft_strdup(current->content);
-	current->content += (ft_strlen(current->content) + 1);
-	free(*line);
+	if (ft_strchr(*line, '\n') != NULL)
+		*ft_strchr(*line, '\n') = '\0';
+	if (ft_strlen(*line) < ft_strlen(current->content))
+	{
+		temp = current->content;
+		current->content = ft_strdup(current->content + ft_strlen(*line) + 1);
+		free(temp);
+	}
+	else
+		ft_strclr(current->content);
 	return (1);
 }
 
-int		main(void) {
+int		main(void)
+{
 	int fd;
-//	int fd2;
+	int fd2;
+	fd = open("gnl3_3.txt", O_RDONLY);
+	fd2 = open("testt2.txt", O_RDONLY);
 	char *line;
-	int x;
-	int ret;
-
-	fd = open("gnl1_3.txt", O_RDONLY);
-//	fd2 = open("testt2.txt", O_RDONLY);
-//	int i = 0;
-	x = 0;
-	while ((ret = get_next_line(fd, &line)) > 0) {
+	int i = 0;
+	while (get_next_line(fd, &line))
+	{
 		ft_putendl(line);
-		x++;
+		free(line);
+		i++;
+		if (i == 2)
+			break ;
 	}
-	printf("x: %d", x);
-//	while (1)
-//	{
-//
-//	}
-//	i = 0;
-//	while (1) {
-//		get_next_line(fd2, &line);
-//		ft_putendl(line);
-//		free(line);
-//		i++;
-//		if (i == 3)
-//			break ;
-//	}
+	i = 0;
+	while (get_next_line(fd2, &line))
+	{
+		ft_putendl(line);
+		free(line);
+		i++;
+		if (i == 2)
+			break;
+	}
+	i = 0;
+	while (get_next_line(fd, &line))
+	{
+		ft_putendl(line);
+		free(line);
+		i++;
+		if (i == 2)
+			break ;
+	}
 }
--- get_next_line.h
+++ get_next_line.h
@@ -13,11 +13,9 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include "libft.h"
+# include "libft.h"
 
-#define BUFF_SIZE 42
+# define BUFF_SIZE 142
 
 int		get_next_line(const int fd, char **line);
 
